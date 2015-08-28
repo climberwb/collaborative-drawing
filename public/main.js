c33e4193521fceb3fdb1325066895d0283320d12
Warren Kushner <warren.kushner@gmail.com>
2015-08-28 04:01:31 +0000

    added winner and front end clear events

--- public/main.js
+++ public/main.js
@@ -1,81 +1,122 @@
 var socket = io();
 
-var guessBox;
+//////// PERSON CONSTRUCTOR
 
-var addGuess = function(guess){
-    $('#guesses ul').append( "<li>"+guess+"</li>" );
-};
-
-var onKeyDown = function(event) {
-    if (event.keyCode != 13) { // Enter
-        return;
-    }
-    addGuess(this.value);
-    socket.emit('guess',this.value);
-    this.value = '';
-    
-};
-
-socket.on('guess',addGuess);
-
-var pictionaryEvent = function(){
-    
-    var canvas, context;
-    var draw = function(pos) {
-        context.beginPath();
-        context.arc(pos.x, pos.y,
-                         6, 0, 2 * Math.PI);
-        context.fill();
-    };
-     canvas = $('canvas');
-     context = canvas[0].getContext('2d');
-     canvas[0].width = canvas[0].offsetWidth;
-     canvas[0].height = canvas[0].offsetHeight;
-     var offset = canvas.offset();
-
-      socket.on('draw',draw);
+var User = function(Id,word){
+    this.Id = Id;
+    this.word = word;
 }
 
-var pictionary = function() {
+var player;
+var setPlayer =function(Id,word){
     
-    var canvas, context, drawing;
+   player =  new User(Id,word);
+   player.playGame();
+}
 
+socket.on('person',setPlayer);
+
+
+User.prototype.playGame = function(){
+    var self = this;
+    ////////////////////////// Show word
+    if(this.word !== null){
+        console.log(this.word);
+        $('#word span').append( this.word );
+        $('#guess').hide();
+        $('#word').show();
+    }else{
+        $('#word').hide();
+        $('#guess').show();
+    }
+    ////////////////////////// GUESS BOX
+    var guessBox;
     
-    var draw = function(position) {
-        context.beginPath();
-        context.arc(position.x, position.y,
-                         6, 0, 2 * Math.PI);
-        context.fill();
+    var addGuess = function(guess){
+        $('#guesses ul').append( "<li>"+guess+"</li>" );
     };
-
-    canvas = $('canvas');
-    context = canvas[0].getContext('2d');
-    canvas[0].width = canvas[0].offsetWidth;
-    canvas[0].height = canvas[0].offsetHeight;
-    canvas.on('mousedown',function(){drawing = true
-        canvas.on('mouseup',function(){drawing = false;});
-            canvas.on('mousemove', function(event) {
-                if(drawing){
-                    var offset = canvas.offset();
-                    var position = {x: event.pageX - offset.left,
-                                    y: event.pageY - offset.top};
-                    socket.emit('draw', position);
-                    draw(position);
-                }
+    
+    var onKeyDown = function(event) {
+        if (event.keyCode != 13) { // Enter
+            return;
+        }
+        //addGuess(this.value);
+        socket.emit('guess',this.value,self.Id);
+        this.value = '';
+        
+    };
+    
+    socket.on('guess',addGuess);
+    
+    var pictionaryEvent = function(){
+        
+        var canvas, context;
+        var draw = function(pos) {
+            context.beginPath();
+            context.arc(pos.x, pos.y,
+                             6, 0, 2 * Math.PI);
+            context.fill();
+        };
+         canvas = $('canvas');
+         context = canvas[0].getContext('2d');
+         canvas[0].width = canvas[0].offsetWidth;
+         canvas[0].height = canvas[0].offsetHeight;
+         var offset = canvas.offset();
+    
+          socket.on('draw',draw);
+    }
+    
+    var pictionary = function() {
+        
+        var canvas, context, drawing;
+    
+        
+        var draw = function(position) {
+            context.beginPath();
+            context.arc(position.x, position.y,
+                             6, 0, 2 * Math.PI);
+            context.fill();
+        };
+    
+        canvas = $('canvas');
+        context = canvas[0].getContext('2d');
+        canvas[0].width = canvas[0].offsetWidth;
+        canvas[0].height = canvas[0].offsetHeight;
+        canvas.on('mousedown',function(){drawing = true
+            canvas.on('mouseup',function(){drawing = false;});
+                canvas.on('mousemove', function(event) {
+                    if(drawing){
+                        var offset = canvas.offset();
+                        var position = {x: event.pageX - offset.left,
+                                        y: event.pageY - offset.top};
+                        socket.emit('draw', position);
+                        draw(position);
+                    }
+                });
             });
+    };
+    
+    // var guessBroadcast = function(){
+        
+    // } 
+    
+    $(document).ready(function() {
+        console.log(player,3);
+        
+        pictionaryEvent();
+        if(self.word !==null){ //remove conditional for collaborative drawing
+            pictionary();
+        }
+        $('#clearCanvas').on('click',function(){//TODO get this event to trigger with socket events
+                                                //after person wins. 
+                                                //TODO only show clar canvas button after a win to the winner.
+            var canvas = $('canvas');
+            var context = canvas[0].getContext('2d');
+            context.clearRect(0, 0, canvas[0].width, canvas[0].height);
         });
-};
+        guessBox = $('#guess input');
+        guessBox.on('keydown', onKeyDown);
+        
+    });
 
-// var guessBroadcast = function(){
-    
-// } 
-
-$(document).ready(function() {
-
-    pictionaryEvent();
-    pictionary();
-    
-    guessBox = $('#guess input');
-    guessBox.on('keydown', onKeyDown);
-    
-});
+}
--- server.js
+++ server.js
@@ -1,28 +1,52 @@
 var http = require('http');
 var express = require('express');
 var socket_io = require('socket.io');
-
+var WORDS = require('./words');
 var app = express();
 app.use(express.static('public'));
 
 var server = http.Server(app);
 var io = socket_io(server);
+var connectionId =0;
+var drawStatus;
+var connectedUsers =[];
 
+
+var wordFind = function(){
+    var index = Math.floor(Math.random() * WORDS.list.length);
+    return WORDS.list[index];
+}
+var players = [];
+var word;
 io.on('connection', function (socket) {
   
-    
+  connectionId++;
+  console.log(connectionId);
+  players.push(connectionId);
+  connectionId === 1 ? drawStatus = true : drawStatus = false;
+  if(drawStatus){
+    word = wordFind();
+  }
+  drawStatus ? socket.emit('person',connectionId, word) : socket.emit('person',connectionId, null);
         
      socket.on('draw', function (coords) {
-      console.log(coords);
+      //console.log(coords);
       socket.broadcast.emit('draw', coords );
       //online = online - 1;
      });
      
-   socket.on('guess',function(guess){
-       socket.broadcast.emit('guess',guess);
+   socket.on('guess',function(guess,Id){
+       console.log(guess===word,word,guess);
+       if(guess === word){
+            io.emit('guess',guess+"CONGRATS "+Id+"!");
+       }else{
+           io.emit('guess',guess);
+       }
    });
 
-  
+  socket.on('disconnect', function() {
+        console.log('A user has disconnected');
+    });
     
     
    // });   
